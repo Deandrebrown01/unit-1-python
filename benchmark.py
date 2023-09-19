93c3f6475e501c2cd901776ed6aa5cc2b8b1027f
deandrebrown01 <dbrown@brooklynsteamcenter.org>
2023-09-19 10:46:33 -0400

    added correct changes

--- benchmark.py
+++ benchmark.py
@@ -1,30 +1,42 @@
 #Exercise 1: Display your name in the console
-print('DeAndre')
+print("DeAndre")
 #Exercise 2: Create a Variable called favoriteShow
 # and set it equal to the name of your favoriteShow
-favoriteShow=('Thevampirediaries')
+favoriteShow='Thevampirediaries'
 #Exercise 3: Create two variables called height and
 #age, then display in the console, the product of them
-height=('5,8')
-age=(16)
-print('DeAndre' + favoriteShow + height + age)
-
+height= 5.8
+age= 16
+print(height* age)
 #Exercise 4: Display "im going to break" in the console
 print('Im going to break')
 
 #Exercise 5: Create a list of strings called "fav_food"
 #put 3 of your favorite food items inside the list
-fav_food=['Chicken'], ['Pizza'], ['Nachos']
+fav_food=['Chicken','Pizza', 'Nachos']
 
 #Exercise 6: Create a conditional statement
+# that checks if your age is greater than or equal to 21
+# if true then display" you can rent a car" other wise
+# display your beat
+if age >= 21:
+    print('You can rent a car')
+else :
+    print('you are beat')
+
 
 #Exercise 7: Ask their user ofr their name, then display 
 #in console""Hello their_name"
-print('Hello what is your name')
+their_name = input(" What is your name? ")
+print("Hello," + their_name)
 #Exercise 8: Create a loop that will display each item 
 #from fav_foods on a seperate line in the console
-
+for i in fav_food :
+    print(i) 
 #Exercise 9: Create a while loop that doesnt run
+x=1
+while x == 2 :
+    print("equal")
 
 
 
