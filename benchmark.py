3279fb58a5f41014727f9bcbe63f60edb91e60eb
deandrebrown01 <dbrown@brooklynsteamcenter.org>
2023-09-19 09:50:45 -0400

    idk how to do some of this

---
+++ benchmark.py
@@ -0,0 +1,30 @@
+#Exercise 1: Display your name in the console
+print('DeAndre')
+#Exercise 2: Create a Variable called favoriteShow
+# and set it equal to the name of your favoriteShow
+favoriteShow=('Thevampirediaries')
+#Exercise 3: Create two variables called height and
+#age, then display in the console, the product of them
+height=('5,8')
+age=(16)
+print('DeAndre' + favoriteShow + height + age)
+
+#Exercise 4: Display "im going to break" in the console
+print('Im going to break')
+
+#Exercise 5: Create a list of strings called "fav_food"
+#put 3 of your favorite food items inside the list
+fav_food=['Chicken'], ['Pizza'], ['Nachos']
+
+#Exercise 6: Create a conditional statement
+
+#Exercise 7: Ask their user ofr their name, then display 
+#in console""Hello their_name"
+print('Hello what is your name')
+#Exercise 8: Create a loop that will display each item 
+#from fav_foods on a seperate line in the console
+
+#Exercise 9: Create a while loop that doesnt run
+
+
+
