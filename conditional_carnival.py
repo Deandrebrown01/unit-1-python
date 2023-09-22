09ec4f4200079ac86d3020e0085ab0ee71154cb5
deandrebrown01 <dbrown@brooklynsteamcenter.org>
2023-09-22 10:50:29 -0400

    didnt finish

---
+++ conditional_carnival.py
@@ -0,0 +1,49 @@
+'''
+TASK 1 Even or Odd
+Determine if a number is even or odd.
+'''
+number =input("Give me a number")
+if number % 2:
+    print("It's even")
+else:
+    print("It's odd")
+
+'''
+TASK 2 Positive, Negative, or Zero:
+Determine if a number is positive, negative, or zero.
+
+
+EXTRA CREDIT: Tell the user if they did not enter a valid number
+'''
+number =int(input("give me a number"))
+if number >=0:
+    print("It's positive")
+else :
+    print("The number is negative")
+
+
+
+'''
+TASK 3: Largest of Three
+Find and print the largest of three numbers.
+'''
+a=175
+b=200
+c=400
+
+
+
+
+'''
+TASK 4: Leap Year
+Determine if a year is a leap year or not.
+'''
+leap=input("Give me a year")
+
+'''
+TASK 5: Vowel or Consonant:
+Identify if a character is a vowel or consonant.
+
+EXTRA CREDIT: Tell the user if they did not enter a valid letter
+'''
+vowel= input("Give me a vowel")
