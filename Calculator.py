9d40a6f24939fc64564361f1e2d4eb38af95ca39
deandrebrown01 <dbrown@brooklynsteamcenter.org>
2023-09-26 10:34:40 -0400

    calculator with explaination

---
+++ Calculator.py
@@ -0,0 +1,34 @@
+Number1=float(input("Enter the number you want: "))
+Number=float(input("Enter the second number:"))
+
+choice=input("What operation do you want to do?")
+
+if choice == 'multiplication':
+    print(Number * Number1)
+elif choice =="addition":
+    print(Number + Number1)
+elif choice=='division':
+    print(Number/Number1)
+elif choice=='floor division':
+    print(Number//Number1)
+elif choice== 'Remainder':
+    print(Number%Number1)
+else:
+    print("This operation is not in this code")
+
+# First I made two float variables so that the user can enter two numbers. Then I made another variable called choice which asks the user what operation do they want to use. I used an if statement and then I used if the persons choice equaled to that operation print the two numbers with the operation sign that goes with that operation. I did that for all of them, then for the code to say this operation is not on there I made an else statement. Afterwards I printed this operation is not on this code. This is how I completed this task.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
