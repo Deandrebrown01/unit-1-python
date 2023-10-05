2d29058dd20be71c50246e6876429bedaa1530ce
deandrebrown01 <dbrown@brooklynsteamcenter.org>
2023-10-05 10:53:05 -0400

    for loop

---
+++ flipping_for.py
@@ -0,0 +1,52 @@
+"""
+Exercise 1:
+Write a program to print each character of a given string using a for loop.
+"""
+D='DeAndre'
+for every in D:
+    print(every)
+
+#I made a variable called D with my name in it and then I made a for loop with for every in D print every. this would print every letter
+
+
+"""
+Exercise 2:
+Write a program to calculate the sum of elements in a list using a for loop.
+"""
+sum=[2,3,4,5,6,7,8,9,25]
+A=0
+for every in sum:
+    A = every+A
+    print(A)
+
+#I made a variable named sum which is a list and then I made a variable named A and I made it zero. Then I did a for loop and I made it for the variable that every + A and it added them together.
+
+"""
+Exercise 3:
+Write a program to print the length of each word in a sentence using a for loop and a list.
+"""
+My_Word=("Hi my name is Deandre")
+My_List=My_Word.split(" ")
+for every in My_Word:
+    print(every)
+
+#I Made a vaariable and then for the second variable I made a variable then i split the first variable with a space andd lastly I made a for loop
+
+"""
+Excercise 4:
+Write a program that creates a dictionary (atleast 4 key:value pairs) and then
+iterates through a dictionary and prints the result
+
+
+
+In a comment, answer the following, what do you notice about the output of your code?
+Is it what you expected?
+"""
+dictionary={
+    'Evisu':7,
+    'Edhardy':8
+}
+for every in dictionary:
+    print(every)
+
+#I noticed that it printed out correclty and verticle. i dont know anything else. This is not what I expected.
