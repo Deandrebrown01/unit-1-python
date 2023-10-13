49ce313517f36404cf16d33b7b43634b5247ec6e
deandrebrown01 <dbrown@brooklynsteamcenter.org>
2023-10-13 10:53:11 -0400

    done

---
+++ return_of_the_function.py
@@ -0,0 +1,38 @@
+"""
+Task 1: Calculate the Square of a Number
+Write a function that takes an integer as an argument and returns its square.
+"""
+def my_calc(a):
+    return (a*a)
+
+x=my_calc(7)
+print(x)
+
+
+"""
+Task 2: Calculate the Area of a Rectangle:
+Write a function that takes the length and width of a rectangle and returns its area.
+"""
+def my_area(c,b):
+    return(c*b)
+y= my_area(9,10)
+print(y)
+"""
+Task 3: Convert Temperature from Celsius to Fahrenheit:
+Write a function that takes a temperature in Celsius and returns 
+the equivalent temperature in Fahrenheit using the correct formula
+"""
+def my_temp(d):
+    return(d * 9/5) + 32
+z=my_temp(80)
+print(z)
+
+"""
+Task 4: Calculate the Average of Numbers:
+Write a function that takes a list of numbers 
+and returns the average (mean) of those numbers.
+"""
+def my_ave(k):
+    return(k+10+10+10+10) /5
+j=my_ave(10)
+print(j)
